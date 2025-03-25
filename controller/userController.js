0b4ba5a8a364ebf08780697ffdcba3d703f56936
kalachuuchiiii <parissrowlet@gmail.com>
2025-03-25 14:05:26 +0800

    fixed logout

--- controller/userController.js
+++ controller/userController.js
@@ -96,9 +96,11 @@ const checkSession = async(req, res) => {
 
 const terminateSession = async(req, res) => {
   try {
-    
-    
-    res.clearCookie("session");
+    res.clearCookie("session", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax"
+  });
     
     return res.status(200).json({
       success: true, 
